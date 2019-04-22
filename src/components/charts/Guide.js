8a01b0c2b5a011b21ab7675907c63010e2d74c0e
Jason Kao <jason.kao@columbia.edu>
2019-04-22 15:26:21 -0400

    proportion guide

---
+++ src/components/charts/Guide.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import injectSheet from 'react-jss';
+
+const styles = {
+  strongLine: {
+    stroke: '#555',
+    strokeWidth: 1.8,
+  },
+  line: {
+    stroke: '#888',
+    strokeWidth: 1,
+  },
+  label: {
+    fontFamily: 'Roboto',
+    fontSize: '.85rem',
+  },
+};
+
+// proportion = proportion female
+const Guide = ({ classes, line, upperLimit, proportion, id }) => {
+  if (proportion < 0 || proportion > 1) {
+    console.error('Proportion in guide must be between 0 and 1.');
+    return null;
+  }
+
+  const slope = (1 - proportion) / proportion;
+  let text = `${(proportion * 100).toFixed(0)}% FEMALE`;
+  let x2 = upperLimit;
+  let y2 = upperLimit;
+  if (proportion > 0.5) x2 = y2 / slope;
+  else if (proportion < 0.5) y2 = x2 * slope;
+  else text = 'EQUAL NUMBER OF MEN AND WOMEN';
+
+  return (
+    <g>
+      <path
+        d={line([[0, 0], [x2, y2]])}
+        className={proportion === 0.5 ? classes.strongLine : classes.line}
+        id={id}
+        fill="none"
+        strokeDasharray={proportion === 0.5 ? '5 4' : '4 4'}
+      />
+      <text
+        className={classes.label}
+        transform="translate(14, 14)"
+        fill='#111'
+      >
+        <textPath href={`#${id}`} startOffset={proportion === 0.5 ? '50%' : '22%'} textAnchor="middle">
+          {text}
+        </textPath>
+      </text>
+    </g>
+  );
+};
+
+export default injectSheet(styles)(Guide);
