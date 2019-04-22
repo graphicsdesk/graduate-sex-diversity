0e02d53765709a8d47154f4369ab633bad76fc91
Jason Kao <jason.kao@columbia.edu>
2019-04-21 21:11:41 -0400

    prettier and good night new york

--- src/components/svg/Line.js
+++ src/components/svg/Line.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import fadeExistence from '../charts/fadeExistence';
 
-const Line = ({ d }) => <path d={d} fill="none" stroke="#333" strokeWidth={1.2} />;
+const Line = ({ d }) => (
+  <path d={d} fill="none" stroke="#333" strokeWidth={1.2} />
+);
 
 export default fadeExistence(Line);
