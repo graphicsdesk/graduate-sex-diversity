eea43f2293e4c9ea6cf2fce013b3b69a7f67515d
Jason Kao <jason.kao@columbia.edu>
2019-04-25 15:38:30 -0400

    updated copy for new lede layout

--- src/copy.js
+++ src/copy.js
@@ -1,49 +1,69 @@
 export default `
 This document is written in the markup language ArchieML (AML). AML makes it a lot easier to write and edit text that can be directly structured and used in code.
 
-headline: In a Fat Survey, a New Look at Graduate Student Sex Diversity in Science and Engineering
+headline: In National Science Foundation Data, a New Look at Graduate Student Sex Diversity in Science and Engineering
 
 [lede]
 
-text: Engineering increased a lot.
+text: In 1994, there were 502 male and 70 female graduate students in engineering fields.
+discipline: Engineering
+maxYear: 1994
+showAxesIndicators: true
+
+text: After 2000, the engineering student population saw dramatic amounts of growth, nearly tripling in size before 2010.
+discipline: Engineering
+maxYear: 2010
+[.showGuides]
+* 0.5
+[]
+
+text: However, because the male student population grew at a faster rate than the female one, the percent makeup of females in engineering stayed mostly constant.
+discipline: Engineering
+maxYear: 2010
+[.showGuides] 
+* 0.25
+* 0.5
+* 0.75
+[]
+
+text: After 2010, however, as the growth of the female population accelerated, the line begins to curve toward the gender parity line.
+discipline: Engineering
+maxYear: 2016
+showLine: true
+[.showGuides]
+* 0.25
+* 0.5
+[]
+
+text: That accelerated growth has led to a visible increase in female percent representation among graduate students in engineering fields.
+discipline: Engineering
+showPercentGraph: true
 [.disciplines]
 name: Engineering
 []
 
-text: Among peers, engineering was once last. Now it's above average.
+text: Before 2010, female students were more underrepresented at Columbia than at almost all of its peer institutions. However, after 2010, Columbia rose to above the middle of the pack.
+discipline: Engineering
+showPercentGraph: true
 [.disciplines]
 name: Engineering
 showPeers: true
 []
 
-text: Science increased a bit too, stayed mostly middle of the pack.
+text: As for graduate students in science fields, female representation, with a few twists and turns, has inched towards parity as well.
+discipline: Science
+maxYear: 2016
+showLine: true
+[.showGuides]
+* 0.5
+[]
+
+text: Over the last 20 years, the share of female graduate students in science increased by nearly ten percentage points. Among peer institutions, Columbia has remained about the middle of the pack.
+discipline: Science
+showPercentGraph: true
 [.disciplines]
-name: Engineering
-showPeers: true
 name: Science
 showPeers: true
-[]
-
-text: But every field has its own story.
-[.disciplines]
-name: Engineering
-showPeers: true
-name: Science
-showPeers: true
-[]
-
-text: While fields in biological sciences like genetics and biometry have large proportions of women...
-[.fields]
-name: Biometry and epidemiology
-name: Genetics
-[]
-
-text: Fields like physics and mechanical engineering are seeing a constant or growing underrepresentation of women.
-[.fields]
-name: Biometry and epidemiology
-name: Genetics
-name: Mechanical engineering
-name: Physics
 []
 
 []
@@ -52,7 +72,7 @@ name: Physics
 
 * The Eye analyzed a shit ton of data from the National Science Foundation, nutgraf, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris odio, tempor in consectetur non, molestie nec tellus. Sed eu efficitur mauris. Phasellus luctus nulla quam. Nam id dictum nunc. Suspendisse potenti.
 
-* Broader trends, Advancement of Women, Praesent convallis odio turpis, in feugiat felis sollicitudin nec. Fusce in pharetra est. Phasellus fringilla tempor ornare. Ut augue diam, viverra at vulputate eu, eleifend a diam.
+* Broader trends, Advancement of Women, Praesent convallis odio turpis, in feugiat felis sollicitudin nec. Fusce in pharetra est. Phasellus fringilla tempor ornare. Ut augue diam, viverra at vulputate eu, eleifend a diam. (For details on how we did this analysis, see the note at the end of this article.)
 
 * We’ve found that while the representation of women has continued to be much more pronounced in medicinal and social science fields, the representation of women in many engineering and physical sciences fields has made little, no, or, even backwards progress.
 
@@ -60,57 +80,7 @@ name: Physics
 
 []
 
-{scatters}
+[methodology]
 
-[.mechanical_engineering]
-
-text: In 1994, there were six female and eighty three male graduate students in the mechanical engineering department.
-maxYear: 1994
-showAxesIndicators: true
-
-text: 6.7% of students in the department was female.
-maxYear: 1994
-[.showGuides] 
-* 0.25
-* 0.5
-* 0.75
-[]
-
-text: In the next couple of years, the department neared gender parity due to a shrinking male population.
-maxYear: 2000
-[.showGuides]
-* 0.25
-* 0.5
-* 0.75
-[]
-
-text: However, during the next decade, the male population would grow at a much faster rate than the female population...
-maxYear: 2016
-[.showGuides]
-* 0.5
-[]
-
-text: ...driving Mechanical Engineering away from the gender equality line.
-maxYear: 2016
-showLine: true
-[.showGuides]
-* 0.5
-[]
-
-text: Looking at percentage representation of female students in Mechanical Engineering, Columbia has increased a couple of percentage points.
-showPercentGraph: true
-[.fields]
-name: Mechanical engineering
-[]
-
-text: However, Columbia still lags behind its peers in this department.
-showPercentGraph: true
-[.fields]
-name: Mechanical engineering
-showPeers: true
-[]
-
-[]
-
-{}
+* Data was exported from the NSF’s new interactive tool.
 `;
